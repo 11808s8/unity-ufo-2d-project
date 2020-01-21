51ec74122e05705a71f1ee4c6a4e4376439a7593
Adriano Gomes da Silva <adrianogsss@gmail.com>
2020-01-21 12:21:32 -0300

    feat: added PlayerController.cs script and speed
    Lesson 2 completed.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rigidbody2d;
+
+    public float speed;
+
+
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D> ();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rigidbody2d.AddForce(movement*speed);
+
+    }
+}
