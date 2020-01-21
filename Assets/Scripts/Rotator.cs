07b61c114ec41a08314b3d1d7ff028aa42dfb6e2
Adriano Gomes da Silva <adrianogsss@gmail.com>
2020-01-21 13:51:49 -0300

    feat: Pickups added!
    Rotator Script created too.
    Lesson 6 completed

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float xAngle;
+    public float yAngle;
+    public float zAngle;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(xAngle, yAngle, zAngle) * Time.deltaTime); 
+    }
+}
